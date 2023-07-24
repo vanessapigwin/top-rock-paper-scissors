b5be95f2d4fad3d5fc149ad7c72e02c2d7b451f1
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-07-24 16:05:41 +0800

    added file for ui only

---
+++ create_ui.js
@@ -0,0 +1,95 @@
+function createUI() {
+    let mainContainer = document.querySelector('.main-container');
+    let computerDiv = document.createElement('div');
+    let playerDiv = document.createElement('div');
+    let choiceDiv = document.createElement('div');
+
+    // computer
+    let computerStatsDiv = document.createElement('div');
+    let computerHpBarDiv = document.createElement('div');
+    let computerHpDiv = document.createElement('div');
+    let computerP = document.createElement('p');
+    let computerOutcomeDiv = document.createElement('div');
+    let computerImg = document.createElement('img');
+
+    computerP.textContent = 'HP: 5 / 5';
+    computerOutcomeDiv.textContent = 'I\'ll only need one core for you!';
+    computerImg.src = 'images/computer.png';
+
+    computerHpBarDiv.appendChild(computerHpDiv);
+    computerStatsDiv.appendChild(computerHpBarDiv);
+    computerStatsDiv.appendChild(computerP);
+    computerStatsDiv.appendChild(computerOutcomeDiv);
+    computerDiv.appendChild(computerStatsDiv);
+    computerDiv.appendChild(computerImg);
+    mainContainer.appendChild(computerDiv);
+    
+    mainContainer.classList.add('main-container');
+    computerDiv.classList.add('stats-outcome', 'computer-stats');
+    computerStatsDiv.classList.add('computer-stats');
+    computerHpBarDiv.classList.add('hp-bar');
+    computerHpDiv.classList.add('hp');
+
+    // player
+    let playerImg = document.createElement('img');
+    let playerStatsDiv = document.createElement('div');
+    let playerHpBarDiv = document.createElement('div');
+    let playerHpDiv = document.createElement('div');
+    let playerP = document.createElement('p');
+    let playerOutcomeDiv = document.createElement('div');
+
+    playerP.textContent = 'HP: 5 / 5';
+    playerOutcomeDiv.textContent = 'Computer wants to challenge you...';
+    playerImg.src = 'images/dragon.png';
+
+    playerHpBarDiv.appendChild(playerHpDiv);
+    playerStatsDiv.appendChild(playerHpBarDiv);
+    playerStatsDiv.appendChild(playerP);
+    playerStatsDiv.appendChild(playerOutcomeDiv);
+    playerDiv.appendChild(playerImg);
+    playerDiv.appendChild(playerStatsDiv);
+    mainContainer.appendChild(playerDiv);
+
+    playerDiv.classList.add('stats-outcome', 'player-stats');
+    playerStatsDiv.classList.add('player-stats');
+    playerHpBarDiv.classList.add('hp-bar');
+    playerHpDiv.classList.add('hp');
+
+    // selection
+    let buttonContainer = document.createElement('div');
+    let rockDiv = document.createElement('div');
+    let paperDiv = document.createElement('div');
+    let scissorsDiv = document.createElement('div');
+    let rockLink = document.createElement('a');
+    let paperLink = document.createElement('a');
+    let scissorsLink = document.createElement('a');
+    let rockArrow = document.createElement('span');
+    let paperArrow = document.createElement('span');
+    let scissorsArrow = document.createElement('span');
+
+    rockLink.innerHTML = '<span class="arrow">&#9656 </span>Rock';
+    paperLink.innerHTML = '<span class="arrow">&#9656 </span>Paper';
+    scissorsLink.innerHTML = '<span class="arrow">&#9656 </span>Scissors';
+    rockLink.dataset.choice = 'Rock';
+    paperLink.dataset.choice = 'Paper';
+    scissorsLink.dataset.choice = 'Scissors';
+
+    rockDiv.appendChild(rockLink);
+    paperDiv.appendChild(paperLink);
+    scissorsDiv.appendChild(scissorsLink);
+    buttonContainer.appendChild(rockDiv);
+    buttonContainer.appendChild(paperDiv);
+    buttonContainer.appendChild(scissorsDiv);
+    mainContainer.appendChild(buttonContainer);
+
+    rockArrow.classList.add('arrow');
+    paperArrow.classList.add('arrow');
+    scissorsArrow.classList.add('arrow');
+    rockLink.classList.add('rock');
+    paperLink.classList.add('paper');
+    scissorsLink.classList.add('scissors');
+    rockDiv.classList.add('move');
+    paperDiv.classList.add('move');
+    scissorsDiv.classList.add('move');
+    buttonContainer.classList.add('button-container')
+}
--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// const MOVES = ['Rock', 'Paper', 'Scissors'];
-// const rock = document.querySelector('.rock');
-// const paper = document.querySelector('.paper');
-// const scissors = document.querySelector('.scissors');
-
-// let playerSelection;
-// let playerScore = 0;
-// let computerScore = 0;
+const MOVES = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice () {
     let random_number = Math.floor(Math.random()*3)
@@ -27,76 +20,24 @@ function playRound(playerSelection, computerSelection) {
     return winner;
 }
 
-function createUI() {
-    let mainContainer = document.querySelector('.main-container');
-    let computerDiv = document.createElement('div');
-    let playerDiv = document.createElement('div');
-    let choiceDiv = document.createElement('div');
-
-    // computer
-    let computerStatsDiv = document.createElement('div');
-    let computerHpBarDiv = document.createElement('div');
-    let computerHpDiv = document.createElement('div');
-    let computerP = document.createElement('p');
-    let computerOutcomeDiv = document.createElement('div');
-    let computerImg = document.createElement('img');
-
-    computerP.textContent = 'HP: 5 / 5';
-    computerOutcomeDiv.textContent = 'I\'ll only need one core for you!';
-    computerImg.src = 'images/computer.png';
-
-    computerHpBarDiv.appendChild(computerHpDiv);
-    computerStatsDiv.appendChild(computerHpBarDiv);
-    computerStatsDiv.appendChild(computerP);
-    computerStatsDiv.appendChild(computerOutcomeDiv);
-    computerDiv.appendChild(computerStatsDiv);
-    computerDiv.appendChild(computerImg);
-    mainContainer.appendChild(computerDiv);
-    
-    mainContainer.classList.add('main-container');
-    computerDiv.classList.add('stats-outcome', 'computer-stats');
-    computerStatsDiv.classList.add('computer-stats');
-    computerHpBarDiv.classList.add('hp-bar');
-    computerHpDiv.classList.add('hp');
-
-    // player
-    let playerImg = document.createElement('img');
-    let playerStatsDiv = document.createElement('div');
-    let playerHpBarDiv = document.createElement('div');
-    let playerHpDiv = document.createElement('div');
-    let playerP = document.createElement('p');
-    let playerOutcomeDiv = document.createElement('div');
-
-    playerP.textContent = 'HP: 5 / 5';
-    playerOutcomeDiv.textContent = 'Computer wants to challenge you...';
-    playerImg.src = 'images/dragon.png';
-
-    playerHpBarDiv.appendChild(playerHpDiv);
-    playerStatsDiv.appendChild(playerHpBarDiv);
-    playerStatsDiv.appendChild(playerP);
-    playerStatsDiv.appendChild(playerOutcomeDiv);
-    playerDiv.appendChild(playerImg);
-    playerDiv.appendChild(playerStatsDiv);
-    mainContainer.appendChild(playerDiv);
-
-    playerDiv.classList.add('stats-outcome', 'player-stats');
-    playerStatsDiv.classList.add('player-stats');
-    playerHpBarDiv.classList.add('hp-bar');
-    playerHpDiv.classList.add('hp');
-}
-
-// rock.addEventListener('click', () => {
-//     const computerSelection = getComputerChoice();
-//     playerSelection = rock.dataset.choice;
-//     let winner = playRound(playerSelection, computerSelection);
-//     console.log(winner, playerSelection, playerScore, computerSelection, computerScore);
-// })
-
 function startGame() {
     const startScreen = document.querySelector('.start-screen');
     startScreen.style.display = 'none';
 
     createUI();
+
+    const rock = document.querySelector('.rock');
+    const paper = document.querySelector('.paper');
+    const scissors = document.querySelector('.scissors');
+    let playerSelection;
+    let playerHp = 5;
+    let computerHp = 5;
+
+    rock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playerSelection = rock.dataset.choice;
+    let winner = playRound(playerSelection, computerSelection);
+    console.log(winner, playerSelection, playerHp, computerSelection, computerHp);
+})
+
 }
-
-

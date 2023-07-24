473a9f385a07454e31fd0ba2c85738231e634b32
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-07-24 15:23:47 +0800

    player field layout

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function createUI() {
 
     // computer
     let computerStatsDiv = document.createElement('div');
-    let hpBarDiv = document.createElement('div');
-    let hpDiv = document.createElement('div');
+    let computerHpBarDiv = document.createElement('div');
+    let computerHpDiv = document.createElement('div');
     let computerP = document.createElement('p');
     let computerOutcomeDiv = document.createElement('div');
     let computerImg = document.createElement('img');
@@ -45,8 +45,8 @@ function createUI() {
     computerOutcomeDiv.textContent = 'I\'ll only need one core for you!';
     computerImg.src = 'images/computer.png';
 
-    hpBarDiv.appendChild(hpDiv);
-    computerStatsDiv.appendChild(hpBarDiv);
+    computerHpBarDiv.appendChild(computerHpDiv);
+    computerStatsDiv.appendChild(computerHpBarDiv);
     computerStatsDiv.appendChild(computerP);
     computerStatsDiv.appendChild(computerOutcomeDiv);
     computerDiv.appendChild(computerStatsDiv);
@@ -56,10 +56,33 @@ function createUI() {
     mainContainer.classList.add('main-container');
     computerDiv.classList.add('stats-outcome', 'computer-stats');
     computerStatsDiv.classList.add('computer-stats');
-    hpBarDiv.classList.add('hp-bar');
-    hpDiv.classList.add('hp');
+    computerHpBarDiv.classList.add('hp-bar');
+    computerHpDiv.classList.add('hp');
 
     // player
+    let playerImg = document.createElement('img');
+    let playerStatsDiv = document.createElement('div');
+    let playerHpBarDiv = document.createElement('div');
+    let playerHpDiv = document.createElement('div');
+    let playerP = document.createElement('p');
+    let playerOutcomeDiv = document.createElement('div');
+
+    playerP.textContent = 'HP: 5 / 5';
+    playerOutcomeDiv.textContent = 'Computer wants to challenge you...';
+    playerImg.src = 'images/dragon.png';
+
+    playerHpBarDiv.appendChild(playerHpDiv);
+    playerStatsDiv.appendChild(playerHpBarDiv);
+    playerStatsDiv.appendChild(playerP);
+    playerStatsDiv.appendChild(playerOutcomeDiv);
+    playerDiv.appendChild(playerImg);
+    playerDiv.appendChild(playerStatsDiv);
+    mainContainer.appendChild(playerDiv);
+
+    playerDiv.classList.add('stats-outcome', 'player-stats');
+    playerStatsDiv.classList.add('player-stats');
+    playerHpBarDiv.classList.add('hp-bar');
+    playerHpDiv.classList.add('hp');
 }
 
 // rock.addEventListener('click', () => {
